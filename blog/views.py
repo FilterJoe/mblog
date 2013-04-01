85186290d5a144e70de268dda0f9ffa452250b37
Joe Golton <joecode@manycycles.com>
2013-04-01 09:08:15 -0700

    added home page - working locally

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext # to get my css file to be seen
+
+
+def homepage(request):
+    return render_to_response('homepage.html', context_instance=RequestContext(request))
--- mblog/urls.py
+++ mblog/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+from blog.views import homepage
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mblog.views.home', name='home'),
+    url(r'^$', homepage),
     # url(r'^mblog/', include('mblog.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:

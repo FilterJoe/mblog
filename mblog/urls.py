faa1d52bafa957b69e3da7d3941251ba2c945402
Joe Golton <joecode@manycycles.com>
2013-04-01 13:04:22 -0700

    added 500.html and index.html but not sure if that will help remove heroku error messages

--- mblog/urls.py
+++ mblog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from blog.views import homepage
 from django.contrib import admin
+from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,6 +12,6 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    # url(r"^$", TemplateView.as_view(template_name="index.html")),   # quick hack to get things going
     url(r'^admin/', include(admin.site.urls)),
 )

5a2e6ffef891af10aae53f2aa7a351ff78b81b99
Joe Golton <joecode@manycycles.com>
2013-04-02 10:08:51 -0700

    attempt 6 to get static files seen on heroku - requires urls.py change

--- mblog/urls.py
+++ mblog/urls.py
@@ -14,3 +14,10 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:  # Needed in order to get Heroku to find CSS. I don't understand why this is needed.
+    urlpatterns += patterns('',
+                            (r'^static/(.*)$', 'django.views.static.serve', {
+                                'document_root': settings.STATIC_ROOT
+                            }),
+    )

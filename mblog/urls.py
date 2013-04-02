329e9f7e788b94952265b529cbeb5b7ca087a9b7
Joe Golton <joecode@manycycles.com>
2013-04-02 10:14:41 -0700

    attempt 6a to get static files seen on heroku - forgot to import settings

--- mblog/urls.py
+++ mblog/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-from blog.views import homepage
 from django.contrib import admin
+
+from blog.views import homepage
 
 admin.autodiscover()
 
